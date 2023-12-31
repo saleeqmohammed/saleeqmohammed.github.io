fb0daeee483ee29c3a6caf2b28153359d3258edb
Mohammed Saleeq Kolleth <saleeqmohammed7@gmail.com>
2023-12-31 12:54:50 +0000

    icons added

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,4 @@ const text =document.querySelector('.text p');
 text.innerHTML = text.textContent.split('').map(
   (char,i)=>`<span style="transform:rotate(${i*8.3}deg);">${char}</span> `
 ).join('');
+
