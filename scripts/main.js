86029e59dff429ef869954bf2d1d6dd7e1b045f8
Mohammed Saleeq Kolleth <saleeqmohammed7@gmail.com>
2024-01-01 14:43:27 +0000

    Before work redesign

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,24 @@ text.innerHTML = text.textContent.split('').map(
   (char,i)=>`<span style="transform:rotate(${i*8.3}deg);">${char}</span> `
 ).join('');
 
+
+const navLinks = document.querySelectorAll('.navlink');
+const sections = document.querySelectorAll('.section');
+let activeSection = 'home';
+window.addEventListener('scroll',()=>{
+
+  sections.forEach(element => {
+    if(window.scrollY >= (element.offsetTop-element.clientHeight/2)){
+      activeSection = element.id
+    }
+  });
+
+  navLinks.forEach(link=>{
+    if(link.href.includes(activeSection)){
+      document.querySelector('.active-nav').classList.remove('active-nav');
+      link.classList.add('active-nav')
+    }
+  })
+
+
+});
