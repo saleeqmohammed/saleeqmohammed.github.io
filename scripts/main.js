c3a6441f11a5bfefb5e79d8d1950f39849661241
Mohammed Saleeq Kolleth <saleeqmohammed7@gmail.com>
2024-01-01 18:13:06 +0000

    all code done writing pending

--- scripts/main.js
+++ scripts/main.js
@@ -25,19 +25,3 @@ window.addEventListener('scroll',()=>{
 
 
 });
-gsap.registerPlugin(ScrollTrigger)
-const splitTypes =document.querySelectorAll('.watermark')
-splitTypes.forEach((char,i)=>{
-    const text = new SplitType(char,{types:'chars'})
-    gsap.from(text.chars,{
-        scrollTrigger:{
-            trigger:char,
-            start:'top 80%',
-            end:'top 20%',
-            scrub:true,
-            markers:false
-        },
-        opacity:0,
-        stagger: 0.1
-    })
-})
