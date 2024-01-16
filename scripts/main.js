328f53249ecaa5918b14c2f71c0e33b8535130e7
Mohammed Saleeq Kolleth <saleeqmohammed7@gmail.com>
2024-01-16 15:29:00 +0000

    responsive menu done

--- scripts/main.js
+++ scripts/main.js
@@ -25,3 +25,25 @@ window.addEventListener('scroll',()=>{
 
 
 });
+
+function menuClose(params) {
+  document.querySelector('.menu').style.display = 'none';
+  document.querySelector('.blur-background').style.opacity = 0;
+  document.querySelector('.open-menu').style.display = 'block';
+  document.querySelector('.close-menu').style.display = 'none';
+  
+}
+
+let responsiveMenuCheck = document.getElementById('menu-check');
+responsiveMenuCheck.addEventListener('change',()=>{
+  if (responsiveMenuCheck.checked){
+    document.querySelector('.menu').style.display = 'block';
+    document.querySelector('.close-menu').style.display = 'block';
+    document.querySelector('.blur-background').style.opacity = 1;
+    document.querySelector('.open-menu').style.display = 'none';
+    
+  }
+  else{
+    menuClose();
+  }
+})
