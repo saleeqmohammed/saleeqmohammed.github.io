9c194b09c0d063368a3282ac9ccf8f6053bee7e6
saleeqmohammed <saleeqmohammed7@gmail.com>
2024-02-01 18:40:15 +0000

    more animtions added and sections fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
-
+//Rotating wheel code
 const text =document.querySelector('.text p');
 text.innerHTML = text.textContent.split('').map(
   (char,i)=>`<span style="transform:rotate(${i*8.3}deg);">${char}</span> `
 ).join('');
 
+//Desktop Menu
 
 const navLinks = document.querySelectorAll('.navlink');
 const sections = document.querySelectorAll('.section');
@@ -26,24 +27,39 @@ window.addEventListener('scroll',()=>{
 
 });
 
+
+//Mobile menu
+
 function menuClose(params) {
-  document.querySelector('.menu').style.display = 'none';
-  document.querySelector('.blur-background').style.opacity = 0;
-  document.querySelector('.open-menu').style.display = 'block';
-  document.querySelector('.close-menu').style.display = 'none';
+
+    
+let mediaQuery = window.matchMedia('(max-width: 780px)');
+  if (mediaQuery.matches){    
+
+    document.querySelector('.menu').style.display = 'none';
+    document.querySelector('.blur-background').style.opacity = 0;
+    document.querySelector('.open-menu').style.display = 'block';
+    document.querySelector('.close-menu').style.display = 'none';
+  }
   
 }
 
-let responsiveMenuCheck = document.getElementById('menu-check');
-responsiveMenuCheck.addEventListener('change',()=>{
-  if (responsiveMenuCheck.checked){
-    document.querySelector('.menu').style.display = 'block';
-    document.querySelector('.close-menu').style.display = 'block';
-    document.querySelector('.blur-background').style.opacity = 1;
-    document.querySelector('.open-menu').style.display = 'none';
-    
+let mediaQuery = window.matchMedia('(max-width: 780px)');
+if (mediaQuery.matches){      
+  // console.log(mediaQuery.matches);
+  let responsiveMenuCheck = document.getElementById('menu-check');
+  responsiveMenuCheck.addEventListener('change',()=>{
+                                                      if (responsiveMenuCheck.checked){
+                                                        document.querySelector('.menu').style.display = 'block';
+                                                        document.querySelector('.close-menu').style.display = 'block';
+                                                        document.querySelector('.blur-background').style.opacity = 1;
+                                                        document.querySelector('.open-menu').style.display = 'none';
+                                                        
+                                                      }
+                                                      else{
+                                                        menuClose();
+                                                      }
+                                                    })
   }
-  else{
-    menuClose();
-  }
-})
+
+
